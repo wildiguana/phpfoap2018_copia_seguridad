28432be9709a800da7d42f769fd1459ba4966118
foap <foap@BI101-08.epsevg.upc.es>
2019-04-11 13:21:16 +0200

    p

---
+++ ejercicio 4 funciones.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ejercicio  php: 4 funciones</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+<p> <h1>hola</h1></p>
+	<?php 
+	
+    ?>
+</body>
+</html>
