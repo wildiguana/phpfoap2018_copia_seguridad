ec50dde6ba5182c171f712c7546a6096ade81fda
foap <foap@BI101-08.epsevg.upc.es>
2019-04-26 10:30:21 +0200

    none

---
+++ ejercicios/ejercicio 2 calculadora1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ejercicio 2: calculadora 1</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="../css/estilo.css">
+</head>
+<body>
+<p> <h1>Ejercicio 2</h1></p>
+<p>calculadora 1</p>
+
+<form method="post">
+        numero 1:<input type="text" name="num1">
+            <input type="submit" name="enviar" value="enviar">
+    </form>
+
+<?php
+
+
+  ?>
+  </body>
+  </html>
